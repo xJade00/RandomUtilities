531bab127028444be9945f3e42d84645c652de39
xaanit <shadowjacob1@gmail.com>
2020-03-13 00:03:09 -0700

    Result docs, deleting all un-used classes.

--- result/src/main/java/it/xaan/random/result/Result.java
+++ result/src/main/java/it/xaan/random/result/Result.java
@@ -17,143 +17,167 @@
  */
 package it.xaan.random.result;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 
-@SuppressWarnings({"unused", "unchecked", "WeakerAccess"})
-public class Result<T> {
+/**
+ * Represents a Result. This can be thought of as a more expansive {@link java.util.Optional}. This
+ * can be in one of three states: <br>
+ * - Success <br>
+ * - Error <br>
+ * - Empty <br>
+ * <br>
+ * These states can be interacted with the methods {@link #onSuccess(Consumer)}, {@link
+ * #onError(Class, Consumer)}, and {@link #onEmpty(Runnable)} respectively.
+ *
+ * @since 1.0.0
+ */
+@SuppressWarnings({"unused", "unchecked", "WeakerAccess", "null"})
+public final class Result<T> {
 
   private final T element;
   private final Object error;
 
-  private static List<String> autoTemplates = new ArrayList<>();
-  static final ResultTemplate GLOBAL_TEMPLATING = new ResultTemplate();
-  private final ResultTemplate localTemplating = new ResultTemplate();
-
   private Result(@Nullable final T element, @Nullable final Object error) {
-    if (element != null && error != null) {
-      throw new IllegalStateException(
-          "Both element and error can't be non-null. At least one must be null.");
-    }
     this.element = element;
     this.error = error;
-    applyAutos();
   }
 
   // Creation
   private static <U> Result<U> from(@Nullable U element, @Nullable Object error) {
+    if (element != null && error != null) {
+      // This should literally never happen. Unless the user is making some sort of reflection
+      // hacks.
+      throw new IllegalStateException(
+          "Both element and error can't be non-null. At least one must be null. This shouldn't be possible. Please make an issue on the github.");
+    }
     return new Result<>(element, error);
   }
 
+  /**
+   * Creates a new {@link Result} in a Success state with the specified non-null element. If your
+   * element is possibly-null see {@link #ofNullable(Object)}
+   *
+   * @param element The element for this Result.
+   * @param <U> The type of the result.
+   * @return An instance of Result in the Success state.
+   * @since 1.0.0
+   */
   public static <U> Result<U> of(U element) {
-    return from(element, null);
+    return from(Objects.requireNonNull(element), null);
   }
 
+  /**
+   * Creates a new {@link Result} in the Success state or the empty state.
+   *
+   * @param element The possibly-null element for this Result.
+   * @param <U> The type of the result.
+   * @return An instance of Result in the Success state if the element is non-null, otherwise it'll
+   *     be in the Empty state.
+   * @since 1.0.0
+   */
   public static <U> Result<U> ofNullable(@Nullable U element) {
     return element == null ? empty() : of(element);
   }
 
+  /**
+   * Creates a new {@link Result} in the Error state.
+   *
+   * @param error The error for this Result.
+   * @param <U> The type of the Result.
+   * @return An instance of Result in the Error state.
+   * @since 1.0.0
+   */
   public static <U> Result<U> error(Object error) {
-    return from(null, error);
+    return from(null, Objects.requireNonNull(error));
   }
 
+  /**
+   * Creates a new {@link Result} in the Empty state.
+   *
+   * @param <U> The type of the Result.
+   * @return An instance of Result in the Empty state.
+   * @since 1.0.0
+   */
   public static <U> Result<U> empty() {
     return from(null, null);
   }
 
-  // Templating
-  public static void auto(String key) {
-    if (GLOBAL_TEMPLATING.exists(key)) return;
-    autoTemplates.add(key);
-  }
-
-  public static void addTemplate(TemplateBuilder builder) {
-    builder.build(TemplateScope.GLOBAL, null);
-  }
-
-  public void addTemplate(TemplateScope scope, TemplateBuilder builder) {
-    builder.build(scope, this);
-  }
-
-  public boolean exists(String key, TemplateScope scope) {
-    switch (scope) {
-      case BOTH:
-        return exists(key, TemplateScope.GLOBAL) && exists(key, TemplateScope.LOCAL);
-      case GLOBAL:
-        return GLOBAL_TEMPLATING.exists(key);
-      case LOCAL:
-        return localTemplating.exists(key);
-    }
-    throw new IllegalArgumentException("Scope can't be null");
-  }
-
-  public void applyTemplates(TemplateScope scope, String... keys) {
-    if (keys.length == 0) {
-      return;
-    }
-    if (scope == TemplateScope.BOTH) {
-      applyTemplates(TemplateScope.LOCAL, keys);
-      applyTemplates(TemplateScope.GLOBAL, keys);
-    }
-    ResultTemplate template;
-    switch (scope) {
-      case LOCAL:
-        template = getLocalTemplating();
-        break;
-      case GLOBAL:
-        template = GLOBAL_TEMPLATING;
-        break;
-      default:
-        throw new IllegalStateException(
-            "If you see this then something is seriously wrong. Like seriously wrong. It physically should not be allowed here. It should stackoverflow before getting here.");
-    }
-    for (String key : keys) {
-      if (!exists(key, TemplateScope.BOTH)) continue;
-      if (isSuccess()) {
-        template.success(key, element.getClass()).ifPresent(cons -> cons.accept(element));
-      }
-      if (isEmpty()) {
-        template.empty(key).ifPresent(Runnable::run);
-      }
-      if (isError()) {
-        template.error(key, error.getClass()).ifPresent(cons -> cons.accept(error));
-      }
-    }
-  }
-
   // Checks
+
+  /**
+   * If the Result is of an Empty state.
+   *
+   * @return true if both {@link #isError()} and {@link #isSuccess()} return false.
+   * @since 1.0.0
+   */
   public boolean isEmpty() {
-    return this.element == null && this.error == null;
+    return !isSuccess() && !isError();
   }
 
+  /**
+   * If the Result is of an Success state.
+   *
+   * @return true if both {@link #isError()} and {@link #isEmpty()} return false.
+   * @since 1.0.0
+   */
   public boolean isSuccess() {
     return this.element != null;
   }
 
+  /**
+   * If the Result is of an Error state.
+   *
+   * @return true if both {@link #isEmpty()} and {@link #isSuccess()} return false.
+   * @since 1.0.0
+   */
   public boolean isError() {
     return this.error != null;
   }
 
-  public <U> boolean isError(Class<? extends U> clazz) {
+  /**
+   * If the Result is of an Error state and the error is the specified {@link Class}
+   *
+   * @param clazz The class to check against.
+   * @return true if it's this Result is in an Error state and the current error is an instance of
+   *     the class.
+   * @since 1.0.0
+   */
+  public boolean isError(Class<?> clazz) {
     return isError() && clazz.isInstance(this.error);
   }
 
   // Methods for doing stuff, chaining
+
+  /**
+   * Executes code when the {@link Result} is in the Success state defined by {@link #isSuccess()}
+   * returning true. The {@link Consumer} takes in the current element, which is guaranteed to be
+   * never null by definition.
+   *
+   * @param cons The consumer to run.
+   * @return The current instance, useful for chaining.
+   * @since 1.0.0
+   */
   public Result<T> onSuccess(Consumer<T> cons) {
-    if (this.element != null) {
+    if (isSuccess()) {
       cons.accept(this.element);
     }
     return this;
   }
 
+  /**
+   * Executes code when the {@link Result} is in the Empty state defined by {@link #isEmpty()}
+   * returning true.
+   *
+   * @param run The runnable to run.
+   * @return The current instance, useful for chaining.
+   * @since 1.0.0
+   */
   public Result<T> onEmpty(Runnable run) {
     if (isEmpty()) {
       run.run();
@@ -161,100 +185,148 @@ public class Result<T> {
     return this;
   }
 
+  /**
+   * Executes code when the {@link Result} is in the Error state defined by {@link #isError(Class)}
+   * returning true. The {@link Consumer} takes in the current error, which is guaranteed to be
+   * never null by definition. <br>
+   * <br>
+   * Please note that if the Result was made by giving a subclass, such as {@link
+   * java.io.ByteArrayInputStream} then calling this method using {@code InputStream.class} as the
+   * class parameter will work. This will always work when using {@code Object.class} as long as the
+   * Result is in an error state. <br>
+   * <br>
+   * Also please note that due to type erasure there is no way for the code to implicitly know the
+   * type parameters of the error, or even if it takes type parameters, without creating a lot of
+   * extra work for everyone involved. The way around this is to do something similar to {@code
+   * r.onError(List.class, (List<String> list) -> list.forEach(System.out::println));}. It's also
+   * good to keep in mind that this will compile and run even if the type parameter is anything but
+   * {@link String}. It'll error with a {@link ClassCastException} when you try and do anything that
+   * is specific to String, rather than generic to {@link Object}. Please contact any library
+   * developer that doesn't specify all possible errors so you don't have to guess.
+   *
+   * @param cons The consumer to run.
+   * @param clazz The class to run on.
+   * @return The current instance, useful for chaining.
+   * @since 1.0.0
+   */
   public <U> Result<T> onError(Class<? extends U> clazz, Consumer<U> cons) {
-    if (clazz.isInstance(this.error)) {
+    if (isError(clazz)) {
       cons.accept((U) this.error);
     }
     return this;
   }
 
   // Functional methods for further control
-  public <U> Result<U> filter(Predicate<T> filter) {
-    if (isEmpty()) {
-      return empty();
-    }
-    if (this.element != null && filter.test(this.element)) {
-      return Result.empty();
-    }
-    return error(this.error);
+
+  /**
+   * Filters the current {@link Result}. The passed {@link Predicate} will only be called when the
+   * Result is in a Success state. If the filter returns true then this will return the current
+   * instance, otherwise it'll return empty. In any other state the current instance is returned.
+   *
+   * @param filter The predicate to test against.
+   * @return An empty Result if the filter returns false and the current Result is in a Success
+   *     state, otherwise the current instance.
+   *     @since 1.0.0
+   */
+  public Result<T> filter(Predicate<T> filter) {
+    return isSuccess() && !filter.test(element) ? empty() : this;
   }
 
+  /**
+   * Maps the current {@link Result} to a new Result. The passed {@link Function} will only be
+   * called when the Result is in a Success state. If the function returns null this will return an
+   * empty instance, otherwise it returns a new Result with the returned element. In any other state
+   * a new Result is returned with the same information.
+   *
+   * @param func The function to map from the current element to a new one.
+   * @param <U> The type of the new Result.
+   * @return If the current Result is in the Success state, new Result in the Empty state if the
+   *     function returns null, or a Success state with the new element. In any other state a new
+   *     Result is returned with the same information.
+   *     @since 1.0.0
+   */
   public <U> Result<U> map(Function<? super T, U> func) {
     if (isEmpty()) {
       return empty();
-    }
-    if (this.element != null) {
+    } else if (isSuccess()) {
       return ofNullable(func.apply(this.element));
+    } else {
+      // IntelliJ disagrees but it's not empty or a success, it's an error.
+      return error(Objects.requireNonNull(this.error, "Should never happen. If this ever happens the world is ending."));
     }
-    return error(this.error);
   }
 
+  /**
+   * Flatmaps the current {@link Result} to a new Result. The passed {@link Function} will only be
+   * called when the Result is in a Success state.
+   *
+   * @param func The function to map from the current element to a new one.
+   * @param <U> The type of the new Result.
+   * @return If the current Result is in the Success state, the Result returned from the function.
+   *     In any other state a new Result is returned with the same information.
+   * @since 1.0.0
+   */
   public <U> Result<U> flatMap(Function<T, Result<U>> func) {
     if (isEmpty()) {
       return empty();
-    }
-    if (this.element != null) {
-      return of(func.apply(this.element).element);
-    }
-    return error(this.error);
-  }
-
-  public T get() {
-    if (!isSuccess()) {
-      throw new NoSuchElementException("Get call on non-successful Result.");
-    }
-    return this.element;
-  }
-
-  public @Nullable T orElse(@Nullable T other) {
-    if (!isSuccess()) {
-      return other;
+    } else if (isSuccess()) {
+      return func.apply(this.element);
     } else {
-      return this.element;
+      return error(Objects.requireNonNull(this.error, "Should never happen. If this ever happens the world is ending."));
     }
   }
 
+  /**
+   * Retrieves the current element from this {@link Result} if it's in a Success state, otherwise errors.
+   *
+   * @return The non-null element.
+   * @throws NoSuchElementException If {@link #isSuccess()} returns false.
+   * @since 1.0.0
+   */
+  public T get() {
+    return orElseThrow(() -> new NoSuchElementException("Get call on non-successful Result."));
+  }
+
+  /**
+   * Retrieves the current element from this {@link Result} if it's in a success state, otherwise
+   * returns the passed parameter. It should be noted that while this is marked as nullable, it's
+   * not possible for this to return null unless you pass {@code null} to the parameter.
+   *
+   * @param other The value to default to.
+   * @return The current value, or the parameter passed if this Result isn't in a Success state.
+   * @since 1.0.0
+   */
+  public @Nullable T orElse(@Nullable T other) {
+    return isSuccess() ? get() : other;
+  }
+
+  /**
+   * Retrieves the current element from this {@link Result} if it's in a success state, otherwise
+   * returns the result of the passed {@link Supplier}. It should be noted that while this is marked as nullable, it's
+   * not possible for this to return null unless you the supplier returns null.
+   *
+   * @param supplier The supplier to default to.
+   * @return The current value, or the return value of the supplier passed if this Result isn't in a Success state.
+   * @since 1.0.0
+   */
   public @Nullable T orElseGet(Supplier<T> supplier) {
     return orElse(supplier.get());
   }
 
-  public T orElseThrow(Supplier<? extends Throwable> supplier) throws Throwable {
+  /**
+   * Retrieves the current element from this {@link Result} if it's in a success state, otherwise
+   * throws the supplied exception.
+   *
+   * @param supplier The supplier of the exception.
+   * @param <X> The element, useful for type checking.
+   * @return The element.
+   * @throws X If the Result is in a state other than Success.
+   */
+  public <X extends Throwable> T orElseThrow(Supplier<X> supplier) throws X {
     if (isSuccess()) {
       return element;
     }
     throw supplier.get();
-  }
-
-  // Non public methods
-  ResultTemplate getLocalTemplating() {
-    return this.localTemplating;
-  }
-
-  private void checkAutos() {
-    autoTemplates =
-        autoTemplates.stream().filter(GLOBAL_TEMPLATING::exists).collect(Collectors.toList());
-  }
-
-  private void applyAutos() {
-    checkAutos();
-    autoTemplates.forEach(
-        key -> {
-          if (isEmpty()) {
-            GLOBAL_TEMPLATING.empty(key).ifPresent(Runnable::run);
-          }
-
-          if (isSuccess()) {
-            GLOBAL_TEMPLATING
-                .success(key, this.element.getClass())
-                .ifPresent(cons -> cons.accept(element));
-          }
-
-          if (isError()) {
-            GLOBAL_TEMPLATING
-                .error(key, this.error.getClass())
-                .ifPresent(cons -> cons.accept(error));
-          }
-        });
   }
 
   // Overrides
