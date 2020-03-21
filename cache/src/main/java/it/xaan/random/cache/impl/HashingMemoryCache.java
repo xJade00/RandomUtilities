46146d3e33d4a0a908cc4dc31e0f454471313963
xaanit <shadowjacob1@gmail.com>
2020-03-21 10:51:30 -0700

    Change packaging

--- cache/src/main/java/it/xaan/random/cache/impl/HashingMemoryCache.java
+++ cache/src/main/java/it/xaan/random/cache/impl/HashingMemoryCache.java
@@ -15,8 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package it.xaan.random.cache;
+package it.xaan.random.cache.impl;
 
+import it.xaan.random.cache.Cache;
 import it.xaan.random.core.Pair;
 import java.util.HashMap;
 import java.util.HashSet;
